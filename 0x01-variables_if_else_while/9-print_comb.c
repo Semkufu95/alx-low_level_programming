9459b025d69e6d9fc4b047341ed879e4d5e33602
Semkufu95 <semkufu95@gmail.com>
2023-03-20 14:56:32 -0700

    print combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,9 +12,12 @@ int main(void)
 
 	for (n = 0 ; n < 10; n++)
 	{
-		putchar(n);
-		putchar(',');
-		putchar(' ');
+		putchar(n + '0');
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
