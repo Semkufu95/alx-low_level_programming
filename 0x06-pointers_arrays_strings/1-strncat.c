5be1f388e8eecaa4879a91b1772ce52b500d655a
Semkufu95 <semkufu95@gmail.com>
2023-04-04 06:28:25 -0700

    A function that concatenates 2 strings with no null terminator

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * _strncat - A function that concatenates two strings without null terminator
+  *
+  * @src: source code
+  * @dest: destination code
+  * @n: number of bytes
+  * Return: destination (dest)
+  */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+	while (b < n && src[b] != '\0')
+	{
+		dest[a] = src[a];
+		b++;
+		a++;
+	}
+	return (dest);
+}
