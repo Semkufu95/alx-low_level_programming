db1073f5957c0d9cf1988685cd0b10bebd26d17b
Semkufu95 <semkufu95@gmail.com>
2023-04-04 05:42:00 -0700

    A function that concatenates two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,8 +2,8 @@
 
 /**
   * _strcat - This function concatenate two strings
-  * @src - source string
-  * @dest - destination string
+  * @src: source string
+  * @dest: destination string
   *
   * Return: destination string
   */
@@ -19,7 +19,8 @@ char *_strcat(char *dest, char *src)
 	while (src[b] != '\0')
 	{
 		dest[a] = src[b];
-		a++; b++;
+		a++;
+		b++;
 	}
 	return (dest);
 }
