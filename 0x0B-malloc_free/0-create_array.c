c71a094bc008b511807241ea25acd50e3159d9e5
Semkufu95 <semkufu95@gmail.com>
2023-04-11 14:15:50 -0700

    Function to create an array of characters

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - Function to create an array
+  * @c: character to be printed
+  * Return: a pointer to an array, NULL if size is 0
+  * @size: size of an array
+  */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arry = malloc((sizeof(char)) * size);
+
+	for (s = 0; s < size; s++)
+	{
+	arry[s] = 'c';
+	}
+	return (arry);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory has files that contain  user-defined functions to execute the malloc and free functions.
