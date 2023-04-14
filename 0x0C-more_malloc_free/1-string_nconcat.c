455c92bb3801db5c822e9d9655dbbb5ebd135156
Semkufu95 <semkufu95@gmail.com>
2023-04-13 18:56:10 -0700

    Function to concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,45 +2,37 @@
 #include <stdlib.h>
 
 /**
- * str_nconcat - function to concatenate two strings
+ * string_nconcat - function to concatenate two strings
  * @s1: string one to concatenate
  * @s2: string two to concatenate
  * @n: number of bytes of s2
  * Return: content of s1 followed by s2, and null terminated
  */
 
-char *str_concat(char *s1, char *s2, unsigned int n)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concatn;
-	unsigned int len1, len2;
+	unsigned int len1, len2, x, y;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = len2 = 0;
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1])
 		len1++;
-	while (s2[len2] != '\0')
+	while (s2 && s2[len2])
 		len2++;
-	concatn = malloc(sizeof(char) * len1 + len2 + 1);
-	if (concatn == NULL)
+	if (n < len2)
+		concatn = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		concatn = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!concatn)
 		return (NULL);
-	len1 = len2 = 0;
-	while (s1[len1] != '\0')
+	while (x < len1)
 	{
-		concatn[len1] = s1[len1];
-		len1++;
+		concatn[x] = s1[x];
+		x++;
 	}
-	if (n >= len2)
-	{
-		concatn[len2] = s2[len2];
-		len1++, len2++;
-	}
-	while (len2 == '\0')
-	{
-		s2 = "";
-	}
-	concatn[len1] = '\0';
+	while (n < len2 && x < (len1 + n))
+		concatn[x++] = s2[y++];
+	while (n >= len2 && x < (len1 + len2))
+		concatn[x++] = s2[y++];
+	concatn[x] = '\0';
 	return (concatn);
 }
