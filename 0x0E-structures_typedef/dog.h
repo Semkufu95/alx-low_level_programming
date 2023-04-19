59fb737e21d0c97f29397d992819d88cc57b7e01
Semkufu95 <semkufu95@gmail.com>
2023-04-19 13:31:51 -0700

    A function that initializes variable type dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,27 +18,6 @@ struct dog
 	float age;
 };
 
-/**
-  * new_dog - a function that create a new detail of a struct dog
-  * @name: input name of dog
-  * @age: input age of dog
-  * @owner: input name of dog's owner
-  * Return: NULL if memory allocation fail, pointer to new detail of struct dog
-  */
-/**
-  struct dog *new_dog(char *name, char *owner, float age)
-{
-	struct dog *my_dog;
-
-	my_dog = malloc(sizeof(struct dog));
-	if (my_dog == NULL)
-		return (NULL);
-	my_dog->name = name;
-	my_dog->age = age;
-	my_dog->owner = owner;
-	return (my_dog);
-}
-*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
