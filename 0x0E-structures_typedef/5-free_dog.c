0026887e4beeea274184e52377163db4d7ec793c
Semkufu95 <semkufu95@gmail.com>
2023-04-19 16:30:18 -0700

    Free all memories allocated

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include "stdlib.h"
+
+/**
+  * free_dog - function to free memory allocated for dogs
+  * @d: memory to be freed
+  */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
