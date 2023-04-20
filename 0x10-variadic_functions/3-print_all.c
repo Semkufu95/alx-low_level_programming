a2c826fe9d6e83a53aafaf6c8752d2cd653b6b15
Semkufu95 <semkufu95@gmail.com>
2023-04-20 11:23:43 -0700

    A function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -23,7 +23,7 @@ void print_all(const char * const format, ...)
 		switch (format[i])
 		{
 			case 'c':
-				printf("%s%c", separator, va_arg(args, char));
+				printf("%s%c", separator, va_arg(args, int));
 				break;
 			case 'i':
 				printf("%s%d", separator, va_arg(args, int));
