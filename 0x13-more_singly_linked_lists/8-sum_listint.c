05b9e08d548a1e11753fc76c0c6b095b68174257
Semkufu95 <semkufu95@gmail.com>
2023-05-03 12:26:57 -0700

    A function that returns sum of all data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/** sum_listint - prints the sum of all data
+  *
+  * Return: return 0 if no data found, else return sum
+  */
+
+int sum_listint(listint_t *head)
+{
+	if (head == NULL)
+		return (0);
+	else
+		return (head->n + sum_listint(head->next));
+}
