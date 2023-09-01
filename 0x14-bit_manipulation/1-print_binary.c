faf08241f67961254c2c925eb68337e35dccf679
Semkufu95 <semkufu95@gmail.com>
2023-08-31 20:02:05 -0700

    solved print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,18 +9,9 @@
 
 void print_binary(unsigned long int n)
 {
-	int k;
-	int binary = sizeof(unsigned long int) * 8;
-
-	for (k = binary - 1; k >= 0; k--)
+	if (n > 1)
 	{
-		if ((n >> k) & 1)
-		{
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
+		print_binary(n >> 1);
 	}
+	_putchar((n & 1) + '0');
 }
