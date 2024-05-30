0036584658226202611cf52aab35f2852664c11b
semkufu95 <semkufu95@gmail.com>
2024-05-29 17:32:38 -0700

    More corrections

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,14 @@ def island_perimeter(grid):
     '''A function that returns the perimeter of the island in grid'''
     length = len(grid)
     width = len(grid[0])
-    side = 0
-    corner = 0
+    perimeter = 0
 
     for m in range(length):
         for n in range(width):
             if grid[m][n] == 1:
-                side += 1
+                perimeter += 4
                 if (n > 0 and grid[m][n - 1] == 1):
-                    corner += 1
+                    perimeter -= 2
                 if (m > 0 and grid[m - 1][n] == 1):
-                    corner += 1
-    return ((side * 4) - (corner * 2))
+                    perimeter -= 2
+    return perimeter
